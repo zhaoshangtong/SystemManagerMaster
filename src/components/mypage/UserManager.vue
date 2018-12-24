5b4f69c68649387ac6511af0e606d899e50c3e88
zhao <zhaoshangtong@163.com>
2018-12-24 22:13:44 +0800

    first commit

---
+++ src/components/mypage/UserManager.vue
@@ -0,0 +1,253 @@
+<template>
+  <div class="table">
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <i class="el-icon-lx-cascades"></i> 基础表格
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="container">
+      <div class="handle-box">
+        <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
+        <el-select v-model="select_cate" placeholder="筛选状态" class="handle-select mr10">
+          <el-option key="0" label="全部" value="全部"></el-option>
+          <el-option key="1" label="可用" value="1"></el-option>
+          <el-option key="2" label="不可用" value="0"></el-option>
+        </el-select>
+        <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+        <el-button type="primary" icon="search" @click="search">搜索</el-button>
+      </div>
+      <el-table
+        :data="data"
+        border
+        class="table"
+        ref="multipleTable"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" label="ID" sortable width="150"></el-table-column>
+        <el-table-column prop="userName" label="用户名" sortable width="150"></el-table-column>
+        <el-table-column prop="loginName" label="登录名" width="120"></el-table-column>
+        <el-table-column prop="role" label="角色" width="120"></el-table-column>
+        <el-table-column prop="tel" label="电话" width="220"></el-table-column>
+        <el-table-column prop="status" label="状态"></el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template slot-scope="scope">
+            <el-button
+              type="text"
+              icon="el-icon-edit"
+              @click="handleEdit(scope.$index, scope.row)"
+            >编辑</el-button>
+            <el-button
+              type="text"
+              icon="el-icon-delete"
+              class="red"
+              @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="pagination">
+        <el-pagination
+          background
+          @current-change="handleCurrentChange"
+          layout="prev, pager, next"
+          :total="totalCount"
+        ></el-pagination>
+      </div>
+    </div>
+
+    <!-- 编辑弹出框 -->
+    <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="用户名">
+          <!-- <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="form.date"
+            value-format="yyyy-MM-dd"
+            style="width: 100%;"
+          ></el-date-picker>-->
+          <el-input v-model="form.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="登录名">
+          <el-input v-model="form.loginName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色">
+          <el-input v-model="form.role"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="form.tel"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-input v-model="form.status"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveEdit">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 删除提示框 -->
+    <el-dialog title="提示" :visible.sync="delVisible" width="300px" center>
+      <div class="del-dialog-cnt">删除不可恢复，是否确定删除？</div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="delVisible = false">取 消</el-button>
+        <el-button type="primary" @click="deleteRow">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  name: "userlist",
+  data() {
+    return {
+      url: "./static/userlist.json",
+      tableData: [],
+      totalCount: 10,
+      cur_page: 1,
+      multipleSelection: [],
+      select_cate: "",
+      select_word: "",
+      del_list: [],
+      is_search: false,
+      editVisible: false,
+      delVisible: false,
+
+      form: {
+        id: 0,
+        userName: "",
+        loginName: "",
+        role: "",
+        status: "",
+        tel: ""
+      },
+      idx: -1
+    };
+  },
+  created() {
+    this.getData();
+  },
+  computed: {
+    data() {
+      return this.tableData.filter(d => {
+        let is_del = false;
+        for (let i = 0; i < this.del_list.length; i++) {
+          if (d.userName === this.del_list[i].userName) {
+            is_del = true;
+            break;
+          }
+        }
+        if (!is_del) {
+          if (this.select_cate === "全部") {
+            return this.tableData;
+          } else if (
+            d.status.indexOf(this.select_cate) > -1 &&
+            d.status.indexOf(this.select_word) > -1
+          ) {
+            return d;
+          }
+        }
+      });
+    }
+  },
+  methods: {
+    // 分页导航
+    handleCurrentChange(val) {
+      this.cur_page = val;
+      this.getData();
+    },
+    // 获取 easy-mock 的模拟数据
+    getData() {
+      // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
+      this.$axios
+        .get(this.url, {
+          page: this.cur_page
+        })
+        .then(res => {
+          this.tableData = res.data.list;
+        });
+    },
+    search() {
+      this.is_search = true;
+    },
+    formatter(row, column) {
+      return row.address;
+    },
+    filterTag(value, row) {
+      return row.tag === value;
+    },
+    handleEdit(index, row) {
+      this.idx = index;
+      const item = this.tableData[index];
+      this.form = {
+        id: item.id,
+        userName: item.userName,
+        loginName: item.loginName,
+        role: item.role,
+        status: item.status,
+        tel: item.tel
+      };
+      this.editVisible = true;
+    },
+    handleDelete(index, row) {
+      this.idx = index;
+      this.delVisible = true;
+    },
+    delAll() {
+      const length = this.multipleSelection.length;
+      let str = "";
+      this.del_list = this.del_list.concat(this.multipleSelection);
+      for (let i = 0; i < length; i++) {
+        str += this.multipleSelection[i].name + " ";
+      }
+      this.$message.error("删除了" + str);
+      this.multipleSelection = [];
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    // 保存编辑
+    saveEdit() {
+      this.$set(this.tableData, this.idx, this.form);
+      this.editVisible = false;
+      this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+    },
+    // 确定删除
+    deleteRow() {
+      this.tableData.splice(this.idx, 1);
+      this.$message.success("删除成功");
+      this.delVisible = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.handle-box {
+  margin-bottom: 20px;
+}
+
+.handle-select {
+  width: 120px;
+}
+
+.handle-input {
+  width: 300px;
+  display: inline-block;
+}
+.del-dialog-cnt {
+  font-size: 16px;
+  text-align: center;
+}
+.table {
+  width: 100%;
+  font-size: 14px;
+}
+.red {
+  color: #ff0000;
+}
+</style>
